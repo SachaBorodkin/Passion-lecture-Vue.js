5b1ba61491e720cf31164ec3d18bf09b754b615b
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2026-02-24 15:57:05 +0100

    Revert "feat(ajouter un livre)"
    This reverts commit 2862c60a8db10738f2f46149bfa982bc5b58d119.

--- passion-lecture/src/router/index.js
+++ passion-lecture/src/router/index.js
@@ -1,14 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Login from '../views/LoginVue.vue' // Your existing login
-import SignUp from '../views/SignUp.vue' // The new component
-import AddView from '../views/AddBook.vue' // The new component
+import Login from '../views/LoginVue.vue'
+import SignUp from '../views/SignUp.vue'
+import BookPageView from '../views/BookPageView.vue'
+import ListView from '@/views/ListView.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/login', name: 'login', component: Login },
   { path: '/register', name: 'register', component: SignUp },
-  { path: '/addBook', name: 'addBook', component: AddView },
+  {
+    path: '/book/:id',
+    name: 'BookDetails',
+    component: BookPageView,
+    props: true,
+  },
+  { path: '/list', name: 'list', component: ListView },
 ]
 
 const router = createRouter({
