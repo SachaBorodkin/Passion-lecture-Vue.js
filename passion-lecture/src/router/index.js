2862c60a8db10738f2f46149bfa982bc5b58d119
Damien-16 <pv77ngi@eduvaud.ch>
2026-02-24 15:47:36 +0100

    feat(ajouter un livre)

--- passion-lecture/src/router/index.js
+++ passion-lecture/src/router/index.js
@@ -1,21 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Login from '../views/LoginVue.vue'
-import SignUp from '../views/SignUp.vue'
-import BookPageView from '../views/BookPageView.vue'
-import ListView from '@/views/ListView.vue'
+import Login from '../views/LoginVue.vue' // Your existing login
+import SignUp from '../views/SignUp.vue' // The new component
+import AddView from '../views/AddBook.vue' // The new component
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/login', name: 'login', component: Login },
   { path: '/register', name: 'register', component: SignUp },
-  {
-    path: '/book/:id',
-    name: 'BookDetails',
-    component: BookPageView,
-    props: true,
-  },
-  { path: '/list', name: 'list', component: ListView },
+  { path: '/addBook', name: 'addBook', component: AddView },
 ]
 
 const router = createRouter({
